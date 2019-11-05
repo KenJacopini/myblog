c1dee01b33436aa8fd93ef07b34262391f7cc6b9
KenJacopini <kengranero@gmail.com>
2019-11-05 09:47:13 +0100

    updated post variable

--- bloggapp/views.py
+++ bloggapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
+
 def post_list(request):
+    posts = Post.objects.filter(published_date_lte=timezone.now().order_by_('published_date'))
     return render(request, 'bloggapp/post_list.html', {})
